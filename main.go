bd359478ae1a20b4f1f5bcdb82213882f6e23cf6
jjusann <jjusann@pm.me>
2026-07-09 15:38:16 +0200

    Max length, json parsing and profanities

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "net/http"
 	"sync/atomic"
 	"fmt"
+	"encoding/json"
+	"strings"
 )
 
 type apiConfig struct {
@@ -30,6 +32,50 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hits reset"))
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	response := map[string]string{"error": msg}
+	data, _ := json.Marshal(response)
+	w.Write(data)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to marshal JSON: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+	w.Write(data)
+}
+
+// helper function to replace profanity in chirp body with asterisks
+func replaceProfaneWords(text string) string {
+	// list of prafane workds to filter out
+	profaneWords := map[string]bool{
+		"kerfuffle": true,
+		"sharbert": true,
+		"fornax": true,
+		// Add more profane words as needed
+	}
+	
+	words := strings.Split(text, " ")
+	
+	for i, word := range words {
+
+		lowerWord := strings.ToLower(word)
+		if profaneWords[lowerWord] {
+			words[i] = "****" // replace profane word with asterisks
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+
 func main() {
 
     //Create a new ServeMux
@@ -38,11 +84,23 @@ func main() {
 
 
 	//add readiness endpoint
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})	
+
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        html := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, apiCfg.fileserverHits.Load())
+        w.Write([]byte(html))
+    })
 
 	// add a file server handler for the root path
 	fileServer := http.FileServer(http.Dir("."))
@@ -51,10 +109,35 @@ func main() {
 	//add logo to be accessible optional as it is being already served
 	//mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
 
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
+		// read the request body
+		var request struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&request); err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid request body")
+			return
+		}
+
+		// check if the chirp is too long
+
+		const maxChirpLength = 140
+		if len(request.Body) > maxChirpLength {
+			respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+			return
+		}
+
+		// check for profanity
+		cleanedBody := replaceProfaneWords(request.Body)
+
+		//Respond with the cleaned chirp body
+		respondWithJSON(w, http.StatusOK, map[string]string{"cleaned_body": cleanedBody})
+	})
 
     //Create a new Server struct
     server := http.Server{
